a7f05d812620002247afd9b17231535646771c20
marcelo9807 <139481370+marcelo9807@users.noreply.github.com>
2025-12-01 21:23:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ import repository from './repository/index.js';
 async function main() {
   await connectDB()
   await sequelize.sync({ alter: true });
-
-  await repository.hobbies.create({
-    label:"Treinar"
-  }) 
+  await repository.users.create({
+    cpf:"06562666945",
+    notification:true,
+    fullname:"Marcelo Machado",
+    email:"Marcelo_mch@outlook.com",
+    phone:"41985034355",
+    password:"98071011",
+    birthdata:"07031998"
+  })
     await repository.hobbies.create({
     label:"Ler"
   }) 
